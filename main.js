8976b7da4cc80631944d7564579af8173bcad80f
JohnyUk <johny360@mail.ru>
2021-03-16 22:27:09 +0300

    fetch added

--- main.js
+++ main.js
@@ -8,19 +8,11 @@ fetch(RSS_URL)
     const items = data.querySelectorAll("item");
     let html = ``;
     items.forEach(el => {
-      html += `
-        <article>
-          <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
-          <h2>
-            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
-              ${el.querySelector("title").innerHTML}
-            </a>
-          </h2>
-        </article>
-      `;
+      
     });
     document.body.insertAdjacentHTML("beforeend", html);
     });
+    
     
 
 
